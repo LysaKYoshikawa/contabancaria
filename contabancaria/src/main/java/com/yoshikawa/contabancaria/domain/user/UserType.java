45992af1f44b0f3b782ff1f4c37c195e1bab00ae
Monalysa <monalysaklauck@gmail.com>
2024-01-15 01:07:07 -0300

    first-commit

---
+++ contabancaria/src/main/java/com/yoshikawa/contabancaria/domain/user/UserType.java
@@ -0,0 +1,6 @@
+package com.yoshikawa.contabancaria.domain.user;
+
+public enum UserType {
+    COMMON,
+    MERCHANT
+}
---
+++ contabancaria/src/test/java/com/yoshikawa/contabancaria/ContabancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoshikawa.contabancaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContabancariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
