d3bf307d59b8ddf17fbaf9f765ed42d35a2fc9ae
Monalysa <monalysaklauck@gmail.com>
2024-03-04 23:37:16 -0300

    feat: add test of the transacao

---
+++ contabancaria/src/test/java/com/yoshikawa/contabancaria/services/TransactionServiceTest.java
@@ -0,0 +1,19 @@
+package com.yoshikawa.contabancaria.services;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionServiceTest {
+
+    @Test
+    @DisplayName("Deve criar a transação com sucesso quando tudo estiver OK")
+    void createTransactionSucess() {
+    }
+
+    @Test
+    @DisplayName("Deve criar a Exception quando a trasação estiver permitida")
+    void createTransactionEmpty() {
+    }
+}
